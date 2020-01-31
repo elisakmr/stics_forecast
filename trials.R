f5e5170dc3d2dee53191758770646418a2d8b5fa
Elisa Kamir <elisa.kamir@outlook.fr>
2020-01-31 11:42:06 +0100

    new

--- trials.R
+++ trials.R
@@ -56,4 +56,127 @@ for (year in an){
 
 apply(ref_original[18,,],1,sum)
 
+#################################################################
+
+                                               ## GRIDDED DET SCORES ##
+
+
+acc_prev <- vector()
+msess <- vector()
+
+nom_ref <- paste0(indicateur,b,"_ref.RData")
+nom_prev <- paste0(indicateur,b,"_prev.RData")
+nom_clim <- paste0(indicateur,b,"_clim.RData")
+nom_an_prev <- paste0(indicateur,b,"_anomaly_prev.RData")
+nom_an_clim <- paste0(indicateur,b,"_anomaly_clim.RData")
+nom_an_obs <- paste0(indicateur,b,"_anomaly_obs.RData")
+
+load(file = file.path(dir_data, "extract", nom_ref))
+load(file = file.path(dir_data, "extract", nom_prev))
+load(file = file.path(dir_data, "extract", nom_clim))
+load(file = file.path(dir_data, "extract", nom_an_prev))
+load(file = file.path(dir_data, "extract", nom_an_clim))
+load(file = file.path(dir_data, "extract", nom_an_obs))
+
+ngrids <- dim(array_obs)[1]
+#ndimT <- dim(array_obs)[3]
+
+# ensemble mean
+prev_mean <- apply(array_prev[,,,], c(1,2,4), mean)
+clim_mean <- apply(array_clim[,,,], c(1,2,4), mean)
+
+
+### SEASONAL ###
+
+
+# BE CAREFUL ON AGREGATION METHOD!!!!
+det_seas <- array(NA, dim = c(ngrids,3))
+
+prev_seas <- apply(array_prev[,,,], c(1,2,3), sum) #### aggregation method to be changed!!
+clim_seas <- apply(array_clim[,,,], c(1,2,3), sum)
+ref_seas <- apply(array_obs[,,], c(1,2), sum)
+
+prev_seas_mean <- apply(prev_seas, c(1,2), mean)
+clim_seas_mean <- apply(clim_seas, c(1,2), mean)
+
+anoprev_seas <- apply(prev_anomaly, c(1,2,3), sum)
+anoref_seas <- apply(obs_anomaly, c(1,2), sum)
+
+# MSESS
+
+msess <- veriApply("EnsMsess", fcst=t(prev_seas_mean), fcst.ref=t(clim_seas_mean), obs=t(ref_seas), parallel = TRUE)
+
+# ACC #
+
+acc_prev <- veriApply("EnsCorr", fcst=anoprev_seas, obs=anoref_seas)
+det_seas <- array(c(msess, acc_prev), dim=c(ngrids,2))
+
+nom_prevseas <- paste0(indicateur, b,"scoredetseas_grid_prev.RData")
+
+save(det_seas, file = file.path(dir_data, "score", nom_prevseas))
+
+
+                                              ## BOX DET SCORES ##
+
+
+coeff <- vector()
+seas_ps <- matrix(ncol = 2, nrow = 6)
+
+  load(file = file.path(dir_data, "extract", paste0("lat_",indicateur,".RData")))
+  boot_seas <- list()
+
+    # loading gridded scores
+
+    # seasonal
+    ngrids <- dim(det_seas)[1]
+
+    val_prevseas <- list()
+
+    for (i in 1:ngrids){
+      val_prevseas[[i]] <- det_seas[i,]*cos(pi*lat[i]/180)
+      coeff[i] <- cos(pi*lat[i]/180)
+      # removing NAs
+      if (length(which(is.na(det_seas[i,])))>0){
+        coeff[i] <- NA
+        val_prevseas[[i]] <- rep(NA,2)
+      }
+    }
+
+    boot_seas[[b]] <- apply(array(unlist(val_prevseas) , c(2,ngrids)), 1, sum, na.rm = T)/sum(coeff, na.rm = T)
+
+
+
+
+                                               ### PROB SCORE EXPLORING ###
+## obs
+
+    nom_ref <- paste0(indicateur,b,"_ref.RData")
+    nom_prev <- paste0(indicateur,b,"_prev.RData")
+
+    load(file = file.path(dir_data, "extract", nom_ref))
+    load(file = file.path(dir_data, "extract", nom_prev))
+
+    seas_obs <- apply(array_obs[,,9:20],c(1,2), sum)
+    seas_prev <- apply(array_prev[,,,9:20],c(1,2,3), sum)
+    ps_season <- apply(seas_prev, 2, mean)
+    ref_season <- apply(seas_obs, 2, mean)
+
+    # ps_season_int <- apply(ps_original[,,,9:20], c(1,2,3), sum)
+    # ps_season <- apply(ps_season_int, 2, mean)
+    # ref_season_int <- apply(ref_original[,,9:20], c(1,2), sum)
+    # ref_season <- apply(ref_season_int, 2, mean)
+
+    df_raw <- data.frame()
+    for (y in 1:nan){
+
+      df_raw <- rbind(df_raw, data.frame(type = "prevision", year=an[y], value = ps_season[y]))
+      df_raw <- rbind(df_raw, data.frame(type = "ref", year=an[y], value = ref_season[y]))
+
+    }
+    #aes(fill=type, colour=type)
+    ggplot(df_raw, aes(x=year, y=value,  fill=type)) + geom_bar(stat = "identity", position = "dodge")+
+      labs(title = paste0(indicateur, " seasonal aggregation"), x="Year", y= "Value")+
+      theme_bw()+
+      scale_x_continuous(breaks = scales::pretty_breaks(n = 12))
+
 
